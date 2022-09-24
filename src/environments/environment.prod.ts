a32293ee1810ad5139e3867c9fa687ba45b749be
trentendresen <37137772+trentendresen@users.noreply.github.com>
2022-09-24 10:30:27 -0500

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA1mZf-KQu7bojH4xmnE-RpZgMO37F_m-g",
+    authDomain: "trenter-3f099.firebaseapp.com",
+    projectId: "trenter-3f099",
+    storageBucket: "trenter-3f099.appspot.com",
+    messagingSenderId: "297425232797",
+    appId: "1:297425232797:web:6e97097efbfde83b06c574",
+    measurementId: "G-26GR19DWE1"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA1mZf-KQu7bojH4xmnE-RpZgMO37F_m-g",
+    authDomain: "trenter-3f099.firebaseapp.com",
+    projectId: "trenter-3f099",
+    storageBucket: "trenter-3f099.appspot.com",
+    messagingSenderId: "297425232797",
+    appId: "1:297425232797:web:6e97097efbfde83b06c574",
+    measurementId: "G-26GR19DWE1"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
